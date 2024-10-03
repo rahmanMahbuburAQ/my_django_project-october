aaa0b6809e8e4cd852b4a9bf07b92c14d40f531c
rahmanMahbuburAQ <rahman.muhammadmahbubur@asia-quest.jp>
2024-10-03 11:21:42 +0900

    settings updated

--- my_django_project_october/my_django_project_october/settings.py
+++ my_django_project_october/my_django_project_october/settings.py
@@ -123,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ['.glitch.me', 'localhost', '127.0.0.1']
+
